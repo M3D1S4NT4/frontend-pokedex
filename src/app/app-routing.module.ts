d7e54073d7c1b87351dfc43f31790b2ff1fc7ae5
luisgusu <uluisguju@gmail.com>
2022-04-28 21:28:54 +0000

    Control del registro de usuarios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,18 +4,14 @@ import { AppComponent } from './app.component';
 import { PokedexComponent } from './pokedex/pokedex.component';
 import {LoginComponent} from './user/login/login.component';
 import { RegisterComponent } from './user/register/register.component';
-import { NaturesComponent } from "./pokedex/natures/natures.component";
-import { TypeCalculatorComponent } from "./pokedex/type-calculator/type-calculator.component"
-import { AbilitydexComponent } from "./pokedex/abilitydex/abilitydex.component"
+import {HomeComponent} from './home/home.component';
 
 const routes: Routes = [
-  {path: "", component: PokedexComponent, pathMatch: "full"},
+  {path: "", component: AppComponent, pathMatch: "full"},
   {path: "login", component: LoginComponent, pathMatch: "full"},
   {path: "register", component: RegisterComponent, pathMatch: "full"},
   {path: "pokedex", component: PokedexComponent, pathMatch: "full"},
-  {path: "natures", component: NaturesComponent, pathMatch: "full"},
-  {path: "typeCalculator", component: TypeCalculatorComponent, pathMatch: "full"},
-  {path: "abilities", component: AbilitydexComponent, pathMatch: "full"},
+  {path: "home", component: HomeComponent, pathMatch: "full"},
 ];
 
 @NgModule({
