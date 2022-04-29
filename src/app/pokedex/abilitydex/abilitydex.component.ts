2991a080516a8e42fed49d3177f96682073d1379
PC Gaming <antonio.medina115@alu.ulpgc.es>
2022-04-29 12:00:24 +0100

    Abilitydex v1.5

--- src/app/pokedex/abilitydex/abilitydex.component.ts
+++ src/app/pokedex/abilitydex/abilitydex.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import {HttpErrorResponse} from "@angular/common/http";
+import {NgbTypeahead} from '@ng-bootstrap/ng-bootstrap';
+import {Observable, Subject, merge, OperatorFunction} from 'rxjs';
+import {debounceTime, distinctUntilChanged, filter, map} from 'rxjs/operators';
 import {PokedexService} from "../pokedex.service";
 import { Ability } from "./ability"
 
 @Component({
   selector: 'app-abilitydex',
   templateUrl: './abilitydex.component.html',
-  styleUrls: ['./abilitydex.component.css']
+  styles: ['.form-control { width: 25% !important; }']
 })
 export class AbilitydexComponent implements OnInit {
   public abilities!: Ability[];
+  public names!: string[];
   public length!: number;
+  public model!: any;
+  public searched = '';
 
   constructor(public pokedexService: PokedexService) { }
 
@@ -27,6 +33,40 @@ export class AbilitydexComponent implements OnInit {
       (error: HttpErrorResponse) => {
         alert(error.message);
       }
+    );
+  }
+
+  public getNames(): string[] {
+    let aux = [];
+    for (let i = 0; i < this.abilities.length; i++) {
+      aux[i] = this.abilities[i].name;
+    }
+    return aux;
+  }
+
+  public duplicateModel(modelString: string): void {
+    let aux = '';
+    for (let i = 0; i < this.abilities.length; i++) {
+      if (modelString == this.abilities[i].name) {
+        aux = this.abilities[i].description;
+        this.searched = aux;
+        return;
+      }
+    }
+  }
+
+  @ViewChild('instance', {static: true}) instance!: NgbTypeahead;
+  focus$ = new Subject<string>();
+  click$ = new Subject<string>();
+
+  search: OperatorFunction<string, readonly string[]> = (text$: Observable<string>) => {
+    const debouncedText$ = text$.pipe(debounceTime(200), distinctUntilChanged());
+    const clicksWithClosedPopup$ = this.click$.pipe(filter(() => !this.instance.isPopupOpen()));
+    const inputFocus$ = this.focus$;
+
+    return merge(debouncedText$, inputFocus$, clicksWithClosedPopup$).pipe(
+      map(term => (term === '' ? this.getNames()
+        : this.getNames().filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1)).slice(0, 10))
     );
   }
 }
