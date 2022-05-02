ddfed73598438597b5c851d82bcf9d4fc58b3927
m2artigas <m2artigas@gmail.com>
2022-05-02 12:02:05 +0100

    movedex, en proceso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NaturesComponent } from './pokedex/natures/natures.component';
 import { TypeCalculatorComponent } from './pokedex/type-calculator/type-calculator.component';
 import { AbilitydexComponent } from './pokedex/abilitydex/abilitydex.component';
+import { MovedexComponent } from './pokedex/movedex/movedex.component';
 
 
 @NgModule({
@@ -30,6 +31,7 @@ import { AbilitydexComponent } from './pokedex/abilitydex/abilitydex.component';
     NaturesComponent,
     TypeCalculatorComponent,
     AbilitydexComponent,
+    MovedexComponent,
   ],
   imports: [
     BrowserModule,
