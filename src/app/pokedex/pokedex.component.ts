fac87a82440f293bf4cbdea3224261b8195722d0
fabiohernandez2001 <fabiohmaru@gmail.com>
2022-05-07 16:46:01 +0100

    Control 1

--- src/app/pokedex/pokedex.component.ts
+++ src/app/pokedex/pokedex.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit, ViewChild, AfterViewInit, ChangeDetectorRef} from '@angular/core';
 import { PokeCollapseComponent } from "./poke-collapse/poke-collapse.component";
-
+import { CookieService } from 'ngx-cookie-service';
 @Component({
   selector: 'app-pokedex',
   templateUrl: './pokedex.component.html',
@@ -8,6 +8,9 @@ import { PokeCollapseComponent } from "./poke-collapse/poke-collapse.component";
 })
 export class PokedexComponent {
   selected: string = 'Bulbasaur';
+  constructor(private cookie:CookieService) {
+    cookie.set("user",'');
+  }
   onChanges(name: string) {
     this.selected = name;
   }
