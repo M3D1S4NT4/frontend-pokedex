1516f933aaf80a10a387208b602c76ac90911854
PC Gaming <antonio.medina115@alu.ulpgc.es>
2022-05-14 01:39:17 +0100

    Editor de Pokémon mínimo

--- src/app/team-creator/team-editor/pokemon-editor/pokemon-editor.component.ts
+++ src/app/team-creator/team-editor/pokemon-editor/pokemon-editor.component.ts
@@ -8,11 +8,13 @@ import {debounceTime, distinctUntilChanged, filter, map} from "rxjs/operators";
 import {Pokemon} from "../../../pokedex/pokemon";
 import {Item} from "../../../pokedex/itemdex/item";
 import {Move} from "../../../pokedex/movedex/move";
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-pokemon-editor',
   templateUrl: './pokemon-editor.component.html',
-  styleUrls: ['./pokemon-editor.component.css']
+  styleUrls: ['./pokemon-editor.component.css'],
+  providers: [TitleCasePipe]
 })
 export class PokemonEditorComponent implements OnInit {
   public pokemon!: Pokemon[];
@@ -27,7 +29,7 @@ export class PokemonEditorComponent implements OnInit {
   public move3!: any;
   public move4!: any;
 
-  constructor(public pokedexService: PokedexService) { }
+  constructor(public pokedexService: PokedexService, public titlecasePipe: TitleCasePipe) { }
 
   ngOnInit(): void {
     this.getPokemon();
@@ -72,6 +74,14 @@ export class PokemonEditorComponent implements OnInit {
     for (let i = 0; i < this.pokemon.length; i++) {
       if (this.model == this.pokemon[i].name) {
         let aux = this.pokemon[i].learnset;
+        for (let j = 0; j < aux.length; j++) {
+          if (this.move1 ==  aux[j] || this.move2 ==  aux[j] || this.move3 ==  aux[j] || this.move4 ==  aux[j]) {
+            delete  aux[j];
+            continue;
+          }
+          aux[j] = aux[j].replace("-", " ");
+          aux[j] = this.titlecasePipe.transform(aux[j]);
+        }
         return aux;
       }
     }
@@ -95,6 +105,16 @@ export class PokemonEditorComponent implements OnInit {
       aux[i] = this.items[i].name;
     }
     return aux;
+  }
+
+  deleteFlags(): void {
+    this.model = null;
+    this.ability = null;
+    this.item = null;
+    this.move1 = null;
+    this.move2 = null;
+    this.move3 = null;
+    this.move4 = null;
   }
 
   @ViewChild('instance', {static: true}) instance!: NgbTypeahead;
@@ -139,6 +159,66 @@ export class PokemonEditorComponent implements OnInit {
     return merge(debouncedText$, inputFocus$, clicksWithClosedPopup$).pipe(
       map(term => (term === '' ? this.getPokemonAbilities()
         : this.getPokemonAbilities().filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1)).slice(0, 10))
+    );
+  }
+
+  @ViewChild('instanceMove1', {static: true}) instanceMove1!: NgbTypeahead;
+  focusMove1$ = new Subject<string>();
+  clickMove1$ = new Subject<string>();
+
+  searchMoves1: OperatorFunction<string, readonly string[]> = (text$: Observable<string>) => {
+    const debouncedText$ = text$.pipe(debounceTime(200), distinctUntilChanged());
+    const clicksWithClosedPopup$ = this.clickMove1$.pipe(filter(() => !this.instanceMove1.isPopupOpen()));
+    const inputFocus$ = this.focusMove1$;
+
+    return merge(debouncedText$, inputFocus$, clicksWithClosedPopup$).pipe(
+      map(term => (term === '' ? this.getPokemonMoves()
+        : this.getPokemonMoves().filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1)))
+    );
+  }
+
+  @ViewChild('instanceMove2', {static: true}) instanceMove2!: NgbTypeahead;
+  focusMove2$ = new Subject<string>();
+  clickMove2$ = new Subject<string>();
+
+  searchMoves2: OperatorFunction<string, readonly string[]> = (text$: Observable<string>) => {
+    const debouncedText$ = text$.pipe(debounceTime(200), distinctUntilChanged());
+    const clicksWithClosedPopup$ = this.clickMove2$.pipe(filter(() => !this.instanceMove2.isPopupOpen()));
+    const inputFocus$ = this.focusMove2$;
+
+    return merge(debouncedText$, inputFocus$, clicksWithClosedPopup$).pipe(
+      map(term => (term === '' ? this.getPokemonMoves()
+        : this.getPokemonMoves().filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1)))
+    );
+  }
+
+  @ViewChild('instanceMove3', {static: true}) instanceMove3!: NgbTypeahead;
+  focusMove3$ = new Subject<string>();
+  clickMove3$ = new Subject<string>();
+
+  searchMoves3: OperatorFunction<string, readonly string[]> = (text$: Observable<string>) => {
+    const debouncedText$ = text$.pipe(debounceTime(200), distinctUntilChanged());
+    const clicksWithClosedPopup$ = this.clickMove3$.pipe(filter(() => !this.instanceMove3.isPopupOpen()));
+    const inputFocus$ = this.focusMove3$;
+
+    return merge(debouncedText$, inputFocus$, clicksWithClosedPopup$).pipe(
+      map(term => (term === '' ? this.getPokemonMoves()
+        : this.getPokemonMoves().filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1)))
+    );
+  }
+
+  @ViewChild('instanceMove4', {static: true}) instanceMove4!: NgbTypeahead;
+  focusMove4$ = new Subject<string>();
+  clickMove4$ = new Subject<string>();
+
+  searchMoves4: OperatorFunction<string, readonly string[]> = (text$: Observable<string>) => {
+    const debouncedText$ = text$.pipe(debounceTime(200), distinctUntilChanged());
+    const clicksWithClosedPopup$ = this.clickMove4$.pipe(filter(() => !this.instanceMove4.isPopupOpen()));
+    const inputFocus$ = this.focusMove4$;
+
+    return merge(debouncedText$, inputFocus$, clicksWithClosedPopup$).pipe(
+      map(term => (term === '' ? this.getPokemonMoves()
+        : this.getPokemonMoves().filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1)))
     );
   }
 }
